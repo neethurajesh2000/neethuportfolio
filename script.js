aba29d0284521f54584c0a587f8f255fb8949968
Neethu Mathew <93003036+neethurajesh2000@users.noreply.github.com>
2024-11-19 13:46:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message! I will get back to you shortly.');
+});
